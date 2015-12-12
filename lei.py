d7b527205eb6504f13bba36c86de5b6c1c8934e4
shilinfeng <shilinf@umich.edu>
2015-12-11 22:10:39 -0500

    LEI done

--- lei.py
+++ lei.py
@@ -1,43 +1,52 @@
 from sets import Set
 
-threshold = 1
+threshold = 50
 
 
 def form_trace(history_buffer, start, old, code_cache, exitCodeCacheSet, nextBBLMap, bbls):
     newTrace = []
     traceBBLTgt = Set([])
     prev = start
+    #print old, '??', len(history_buffer)
     for branchId in range(old, len(history_buffer)):
         if prev not in code_cache:
-            #TODO: think about that why pre is not in nextBBLMap
-            if prev in nextBBLMap:
-                exitCodeCacheSet |= Set(nextBBLMap[prev])
-            newTrace.append(branchId)
+            if history_buffer[branchId] in nextBBLMap:
+                exitCodeCacheSet |= Set(nextBBLMap[history_buffer[branchId]])
+            newTrace.append(history_buffer[branchId])
             traceBBLTgt.add(prev)
         if bbls[history_buffer[branchId]][-1] in traceBBLTgt:
             break
         prev = bbls[history_buffer[branchId]][-1]
     code_cache[start] = newTrace
     #print start
+    #print newTrace
+    #print "-------------------"
 
 
 
-def interpreted_branch_taken(countMap, hb_hash, code_cache, history_buffer, src, tgt, exitCodeCacheSet, nextBBLMap, bbls, bblInCache):
+def interpreted_branch_taken(countMap, hb_hash, code_cache, history_buffer, src, tgt, exitCodeCacheSet, nextBBLMap, bbls, bblInCache, numberOfCounters, hitNumber):
     if tgt in code_cache:
         bblInCache[0] += 1
+        hitNumber[0] += 1
         return code_cache[tgt][1:]
     #print tgt
     if tgt in hb_hash:
         old = hb_hash[tgt]
         hb_hash[tgt] = len(history_buffer)-1
+        if old > hb_hash[tgt]:
+            return []
         if int(tgt, 16) <= int(src, 16) or old in exitCodeCacheSet:
             if tgt not in countMap:
+                numberOfCounters[0] += 1
                 countMap[tgt] = 0
             countMap[tgt] += 1
             if countMap[tgt] == threshold:
                 form_trace(history_buffer, tgt, old, code_cache, exitCodeCacheSet, nextBBLMap, bbls)
                 del history_buffer[old:]
                 del countMap[tgt]
+                bblInCache[0] += 1
+                hitNumber[0] += 1      
+                return code_cache[tgt][1:]
     else:
         hb_hash[tgt] = len(history_buffer)-1
     return []
@@ -57,11 +66,15 @@ def read_output(fileName):
     nextBBLMap = {}
     #tgt address -> count
     countMap = {}
+    #number of counters of tgt address
+    numberOfCounters = [0]
     #index of bbl
     exitCodeCacheSet = Set([])
     currentTrace = []
     bblInCache = [0]
     totalBbl = 0
+    idealTraceNumber = 0
+    hitNumber = [0]
     for line in f:
         line = line.rstrip('\n')
         if line.startswith('@'):
@@ -77,6 +90,8 @@ def read_output(fileName):
             if len(currentTrace)>0 and currentTrace[0]==int(line):
                 bblInCache[0] += 1
                 del currentTrace[0]
+                if len(currentTrace) == 0:
+                    idealTraceNumber += 1
                 continue;
             else:
                 currentTrace = []
@@ -88,12 +103,14 @@ def read_output(fileName):
                 currentIndex = int(line)
                 history_buffer.append(int(line))
                 if bbls[bNumber][-1] == bbls[currentIndex][0] and bbls[bNumber][-1] and len(bbls[bNumber][-1])==12:
-                    currentTrace = interpreted_branch_taken(countMap, hb_hash, code_cache, history_buffer, bbls[bNumber][-2], bbls[bNumber][-1], exitCodeCacheSet, nextBBLMap, bbls, bblInCache)
+                    currentTrace = interpreted_branch_taken(countMap, hb_hash, code_cache, history_buffer, bbls[bNumber][-2], bbls[bNumber][-1], exitCodeCacheSet, nextBBLMap, bbls, bblInCache, numberOfCounters, hitNumber)
             else:
                 history_buffer.append(int(line))
     f.close()
     print len(code_cache)
     print bblInCache[0]*1.0/totalBbl
+    print numberOfCounters[0]
+    print idealTraceNumber*1.0/hitNumber[0]
 
 
 read_output('trace.out')
