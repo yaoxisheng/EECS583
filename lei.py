459f454a6afa51cfca8cf8d977e4a44061fee8d9
shilinfeng <shilinf@umich.edu>
2015-12-13 17:52:29 -0500

    simplified

--- lei.py
+++ lei.py
@@ -2,32 +2,29 @@ from sets import Set
 import time
 
 threshold = 50
-hitNumber = 0
-
 
 def form_trace(history_buffer, start, old, code_cache, exitCodeCacheSet, nextBBLMap, bbls):
     newTrace = []
     traceBBLTgt = Set([])
-    prev = start
     for branchId in range(old, len(history_buffer)):
-        prev = bbls[history_buffer[branchId]][0]
+        bblIndex = history_buffer[branchId]
+        prev = bbls[bblIndex][0]
         if prev in code_cache or prev in traceBBLTgt:
+        #if prev in traceBBLTgt:
             break
-        if history_buffer[branchId] in nextBBLMap:
-            exitCodeCacheSet |= nextBBLMap[history_buffer[branchId]]
-        newTrace.append(history_buffer[branchId])
+        exitCodeCacheSet |= nextBBLMap[bblIndex]
+        newTrace.append(bblIndex)
         traceBBLTgt.add(prev)
     code_cache[start] = newTrace
     exitCodeCacheSet.discard(history_buffer[old])
     return
 
 
-
-def interpreted_branch_taken(countMap, hb_hash, code_cache, history_buffer, src, tgt, exitCodeCacheSet, nextBBLMap, bbls, bblInCache):
-    global hitNumber
+'''
+def interpreted_branch_taken(countMap, hb_hash, code_cache, history_buffer, src, tgt, exitCodeCacheSet, nextBBLMap, bbls, bblInCache, hitNumber):
     if tgt in code_cache:
         bblInCache[0] += 1
-        hitNumber += 1
+        hitNumber[0] += 1
         return code_cache[tgt][1:]
     if tgt in hb_hash:
         old = hb_hash[tgt]
@@ -44,17 +41,18 @@ def interpreted_branch_taken(countMap, hb_hash, code_cache, history_buffer, src,
                 del countMap[tgt]
                 del hb_hash[tgt]
                 bblInCache[0] += 1
-                hitNumber += 1
+                hitNumber[0] += 1
                 return code_cache[tgt][1:]
     else:
         hb_hash[tgt] = len(history_buffer)-1
     return []
-
+'''
 
 def read_output(fileName):
     f = open(fileName, 'r')
     bbl = []
     bbls = [bbl]
+    hitNumber = 0
     #list of indexes of bbl
     history_buffer = []
     #tgt address -> index of bbl
@@ -68,7 +66,7 @@ def read_output(fileName):
     #index of bbl
     exitCodeCacheSet = Set([])
     currentTrace = []
-    bblInCache = [0]
+    bblInCache = 0
     totalBbl = 0
     idealTraceNumber = 0
     timeInIsdigits = 0
@@ -84,18 +82,18 @@ def read_output(fileName):
                 bbl[2] = '0'
             bbls.append(bbl)
         elif line.isdigit():
-            start_time = time.time()
+            #start_time = time.time()
             totalBbl += 1
             currentIndex = int(line)
             if len(currentTrace)>0 and currentTrace[0]==currentIndex:
-                bblInCache[0] += 1
+                bblInCache += 1
                 del currentTrace[0]
                 if len(currentTrace) == 0:
                     idealTraceNumber += 1
                 continue;
             else:
                 currentTrace = []
-            timeInIsdigits += time.time() - start_time
+            #timeInIsdigits += time.time() - start_time
             if len(history_buffer)>1:
                 prevIndex = history_buffer[-1]
                 if prevIndex not in nextBBLMap:
@@ -103,22 +101,50 @@ def read_output(fileName):
                 nextBBLMap[prevIndex].add(currentIndex)
                 history_buffer.append(currentIndex)
                 if bbls[prevIndex][-1]=='0' or bbls[prevIndex][-1] == bbls[currentIndex][0]:
-                    currentTrace = interpreted_branch_taken(countMap, hb_hash, code_cache, history_buffer, bbls[prevIndex][-2], bbls[currentIndex][0], exitCodeCacheSet, nextBBLMap, bbls, bblInCache)
+                    #currentTrace = interpreted_branch_taken(countMap, hb_hash, code_cache, history_buffer, bbls[prevIndex][-2], bbls[currentIndex][0], exitCodeCacheSet, nextBBLMap, bbls, bblInCache, hitNumber)
+                    src = bbls[prevIndex][-2]
+                    tgt = bbls[currentIndex][0]
+
+                    if tgt in code_cache:
+                        bblInCache += 1
+                        hitNumber += 1
+                        currentTrace = code_cache[tgt][1:]
+                        continue
+                    if tgt in hb_hash:
+                        old = hb_hash[tgt]
+                        hb_hash[tgt] = len(history_buffer)-1
+                        if old > hb_hash[tgt]:
+                            continue
+                        if int(tgt, 16) <= int(src, 16) or old in exitCodeCacheSet:
+                            if tgt not in countMap:
+                                countMap[tgt] = 0
+                            countMap[tgt] += 1
+                            if countMap[tgt] == threshold:
+                                form_trace(history_buffer, tgt, old, code_cache, exitCodeCacheSet, nextBBLMap, bbls)
+                                del history_buffer[old:]
+                                del countMap[tgt]
+                                del hb_hash[tgt]
+                                bblInCache += 1
+                                hitNumber += 1
+                                currentTrace = code_cache[tgt][1:]
+                                continue
+                    else:
+                        hb_hash[tgt] = len(history_buffer)-1
             else:
                 history_buffer.append(currentIndex)
     f.close()
     print "trace number: " + str(len(code_cache))
-    print "bbl cover ratio: " + str(bblInCache[0]*1.0/totalBbl)
+    print "bbl cover ratio: " + str(bblInCache*1.0/totalBbl)
     print "ideal trace ratio: "+str(idealTraceNumber*1.0/hitNumber)
     totalTraceLength = 0
     for key, value in code_cache.iteritems():
         totalTraceLength += len(value)
     print "average trace length(count in block): " + str(totalTraceLength*1.0/len(code_cache))
-    print("--- %s seconds in digits---" % timeInIsdigits)
+    #print("--- %s seconds in digits---" % timeInIsdigits)
 
 
 start_time1 = time.time()
-read_output('trace.out')
+read_output('ptrace.out')
 print("--- %s seconds ---" % (time.time() - start_time1))
 
 
